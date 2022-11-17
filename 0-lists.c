998d8846cc86aca414e9d1270a2c5f9262298a85
Jo Kar <josi.kelechi@gmail.com>
2022-11-16 19:05:27 -0800

    refactor betty

--- 0-lists.c
+++ 0-lists.c
@@ -1,12 +1,12 @@
 #include "posh.h"
 
 /**
- * add_node - adds a node to the start of the list
+ * add_node - add a node to the start of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
- * Return: size of list
+ * Return: new node added 
  */
 list_t *add_node(list_t **head, const char *str, int num)
 {
@@ -34,12 +34,12 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
+ * add_node_end - add a node to the end of the list
  * @head: address of pointer to head node
  * @str: str field of node
  * @num: node index used by history
  *
- * Return: size of list
+ * Return: new node added
  */
 list_t *add_node_end(list_t **head, const char *str, int num)
 {
@@ -75,7 +75,7 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
+ * print_list_str - print only the str element of a list_t linked list
  * @h: pointer to first node
  *
  * Return: size of list
@@ -95,7 +95,7 @@ size_t print_list_str(const list_t *h)
 }
 
 /**
- * delete_node_at_index - deletes node at given index
+ * delete_node_at_index - delete node at given index
  * @head: address of pointer to first node
  * @index: index of node to delete
  *
@@ -135,10 +135,8 @@ int delete_node_at_index(list_t **head, unsigned int index)
 }
 
 /**
- * free_list - frees all nodes of a list
+ * free_list - free all nodes of a list
  * @head_ptr: address of pointer to head node
- *
- * Return: void
  */
 void free_list(list_t **head_ptr)
 {
