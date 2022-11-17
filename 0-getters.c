ad906f48f547422bb1b8db5abc051ddbe0a67fa5
Jo Kar <josi.kelechi@gmail.com>
2022-11-16 19:01:08 -0800

    say hi to betty

--- 0-getters.c
+++ 0-getters.c
@@ -1,7 +1,7 @@
 #include "posh.h"
 
 /**
- * get_environ - returns the string array copy of our environ
+ * get_environ - return the string array copy of our environ
  * @info: Structure containing potential arguments. Used to maintain
  *          constant function prototype.
  * Return: Always 0
@@ -18,7 +18,7 @@ char **get_environ(info_t *info)
 }
 
 /**
- * _unsetenv - Remove an environment variable
+ * _unsetenv - remove an environment variable
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
  *  Return: 1 on delete, 0 otherwise
@@ -50,7 +50,7 @@ int _unsetenv(info_t *info, char *var)
 }
 
 /**
- * _setenv - Initialize a new environment variable,
+ * _setenv - initialize a new environment variable,
  *             or modify an existing one
  * @info: Structure containing potential arguments. Used to maintain
  *        constant function prototype.
