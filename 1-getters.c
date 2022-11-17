200b1be6de21014538ba3a60d2713ba399c1dafa
Jo Kar <josi.kelechi@gmail.com>
2022-11-16 19:11:04 -0800

    modify code

--- 1-getters.c
+++ 1-getters.c
@@ -1,7 +1,7 @@
 #include "posh.h"
 
 /**
- * clear_info - initializes info_t struct
+ * clear_info - initialize info_t struct
  * @info: struct address
  */
 void clear_info(info_t *info)
@@ -13,7 +13,7 @@ void clear_info(info_t *info)
 }
 
 /**
- * set_info - initializes info_t struct
+ * set_info - initialize info_t struct
  * @info: struct address
  * @av: argument vector
  */
@@ -45,7 +45,7 @@ void set_info(info_t *info, char **av)
 }
 
 /**
- * free_info - frees info_t struct fields
+ * free_info - free info_t struct fields
  * @info: struct address
  * @all: true if freeing all fields
  */
